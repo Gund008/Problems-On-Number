8457dbf11a8bdf51bb479836c3a5c6653572b585
Sukanya <106397577+Gund008@users.noreply.github.com>
2022-12-02 17:56:45 +0530

    Update Display_Even_Factor.c

--- Display_Even_Factor.c
+++ Display_Even_Factor.c
@@ -14,15 +14,7 @@ void DisplayEvenFactor(int iNo)
   {
      iNo=-iNo;
   }
-  /*
-  for(i=1;i<=(iNo/2);i++)
-  {
-    if(((iNo % i)==0)&&((iNo % 2)==0))
-    {
-        printf("%d\t",i);
-    }
-  }
-    */
+  
     for(i=2;i<=(iNo/2);i=i+2)
   {
     if((iNo % i)==0)
