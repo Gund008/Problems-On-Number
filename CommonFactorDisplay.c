1b4ad40f37ea139092b9dd2ddde23636eba71519
Sukanya <106397577+Gund008@users.noreply.github.com>
2022-12-17 23:27:25 +0530

    Add files via upload

---
+++ CommonFactorDisplay.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+/*
+###########################################################################################
+##
+##  Function Name : CommonFactorDisplay
+##  Input : Integer
+##  Output : None
+##  Description : It is used to Display CommonFactor in Two number.
+##  Author : Sukanya Hanumant Gund
+##  Date :17/12/2022
+##
+##########################################################################################
+*/
+
+void CommonFactorDisplay(int iValue1,int iValue2)
+{
+    int iCnt=0;
+
+    printf("Common Factor are:\n");
+    for(iCnt=1;iCnt<=(iValue1/2)&&(iValue2/2);iCnt++)
+    {
+        if((iValue1 % iCnt==0) && (iValue2 % iCnt==0))
+        {
+            printf("%d\n",iCnt);
+        }
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iNo1=0;
+    int iNo2=0;
+    printf("Please Enter First Number:\n");
+    scanf("%d",&iNo1);
+
+    printf("Please Enter Second Number:\n");
+    scanf("%d",&iNo2);
+
+    CommonFactorDisplay(iNo1,iNo2);
+    
+
+    return 0;
+}
